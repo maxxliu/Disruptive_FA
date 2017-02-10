cdaad7efebee0ed9e53dd30a55ec18557d109763
Anne Rogers <amr@cs.uchicago.edu>
2017-02-10 10:56:43 -0600

    dcf_equation

--- data_gathering/dcf_equation.py
+++ data_gathering/dcf_equation.py
@@ -5,16 +5,14 @@ def dcf_calculator(data_dict, risk_factor, expected_return, growth_rate):
     tax_rate = 0.35
     risk_free_rate = 0.0238 #10 year U.S. Treasury Bond
     summary = data_dict['summary quote']
+    income_dict = data_dict['Income Statement']
+    balance_sheet = data_dict['Balance Sheet']
     
     fcf_value_list = []
     pvfcf = 0
     for year in range(4):
 
         beta = summary['beta'][year]
-
-        income_dict = data_dict['Income Statement']
-        balance_sheet = data_dict['Balance Sheet']
-
         gross_profit = income_dict['Total Revenue'][year] - income_dict['COGS'][year]
         EBITDA = gross_profit - income_dict['Selling gneral and Admin Expenses'][year]
         EBIT = EBITDA - income_dict['Research and Development'][year] - income_dict['Non-Recurring Items'][3] - income_dict['Other Operating Items'][3]
@@ -22,7 +20,7 @@ def dcf_calculator(data_dict, risk_factor, expected_return, growth_rate):
         equity = balance_sheet['Total Stockholders Equity'][year]
         total_d_e = debt + equity
         cost_equity = risk_free_rate + beta * (expected_return - risk_free_rate)
-        WACC = (equity/total_d_e) * cost_equity + (debt/ total_d_e) * rd* (1 - tax_rate)
+        WACC = (equity/total_d_e) * cost_equity + (debt/ total_d_e) * rd* (1 - tax_rate) #rd not pulled yet
 
         FCF = EBIT(1 - tax_rate) + depreciation - NWC - Capex #NWC, depreciation, Capex not pulled yet
         
@@ -35,8 +33,8 @@ def dcf_calculator(data_dict, risk_factor, expected_return, growth_rate):
 
 
     enterprise_value = pvfcf + fcf_value_list[-1]
-    equity_value = enterprise_value - debt - preferred_stock - minor_interest + cash
-    price_per_share = equity_value / shares_outstanding
+    equity_value = enterprise_value - debt - preferred_stock - minor_interest + cash #preferred_stock, minor_interest, cash not pulled yet
+    price_per_share = equity_value / shares_outstanding #shares_outstanding not pulled yet
 
     return price_per_share
 
