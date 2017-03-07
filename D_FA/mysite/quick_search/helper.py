1b74ace91d76dbb135833a91f08d4c09e92e100a
Jonathan Li <jonathanl1@cs.uchicago.edu>
2017-03-06 18:22:58 -0600

    more django work

--- D_FA/mysite/quick_search/helper.py
+++ D_FA/mysite/quick_search/helper.py
@@ -9,18 +9,67 @@ def get_recommended(stock, summary):
 
 	Inputs:
 		stock: the queried stock object
-		summary: the queried summary object
+		summary: the queried summary_data object
 
 	Outpus:
 		recommended: a list of recommended stock objects
 	'''
-	pre = Stock.objects.filter(sector = stock.sector, industry = stock.industry)
+	pre = Stock.objects.filter(sector = stock.sector, 
+		industry = stock.industry)
 	
 	recommended = []
 	for obj in pre:
 		current = Summary_Data.objects.get(ticker = obj)
-		if 0.8*summary.market_cap <= current.market_cap <= 1.2*summary.market_cap:
+		if 0.8*summary.market_cap <= current.market_cap \
+		<= 1.2*summary.market_cap:
 			if current.ticker.ticker != stock.ticker:
 				recommended.append(current.ticker.ticker)
 
 	return recommended
+
+
+def format_header(stock, summary):
+	'''
+	Formats the header list of the results page with a stock and summary_data 
+	object
+
+	Inputs:
+		stock: the queried stock object
+		summary: the queried summary_data object
+
+	Outputs:
+		header_list: list of tuples for render dictionary
+	'''
+	pre = []
+	pre2 = []
+	pre.append(stock.ticker + ' | ')
+	pre.append(stock.name + ' | ')
+	pre.append('Previous Close: $' + str(summary.previous_close))
+	pre2.append('High: $' + str(summary.year_high) + ' | ')
+	pre2.append('Low: $' + str(summary.year_low) + ' | ')
+	pre2.append('Beta: ' + str(summary.beta) + ' | ')
+	pre2.append('Market Cap: $' + str(summary.market_cap))
+
+	header_list = [''.join(pre), ''.join(pre2)]
+
+	return header_list
+
+
+def format_dates(data_date):
+	'''
+	Formates the dates list to use in the financials table header with a 
+	data_date object
+
+	Inputs:
+		data_date: the queried data_date object
+
+	Outputs:
+		date_list: list of dates to use in the financials table header
+	'''
+	date_list = []
+	date_list.append(data_date.year_1)
+	date_list.append(data_date.year_2)
+	date_list.append(data_date.year_3)
+	date_list.append(data_date.year_4)
+
+	return date_list
--- D_FA/mysite/quick_search/views.py
+++ D_FA/mysite/quick_search/views.py
@@ -21,21 +21,25 @@ def results(request):
 			# format render dict entry for stock
 
 			summary_data = Summary_Data.objects.get(ticker = stock)
+			header_list = format_header(stock, summary_data)
 			# check for update on summary data values
 			# format render dict entry for summary data
 
 			data_date = Data_Date.objects.get(ticker = stock)
+			date_list = format_dates(data_date)
 			# check for update on fin statemenet values
 
-			fin_statements = Fin_Statement.objects.filter(ticker = stock)	# this a list
+			fin_statements = Fin_Statement.objects.filter(ticker = stock)
 			# calculate DCF and rating from financials list
 			# format render dict entry for financials
 
 			recommended = get_recommended(stock, summary_data)
 
-			return render(request, 'quick_search/results.html', {'stock': [stock.ticker],
-				'summary_data': [summary_data.previous_close, summary_data.updated],
-				'recommended': recommended})		
+			return render(request, 'quick_search/results.html', 
+				{'header': header_list,
+				'dates': date_list,
+				'recommended': recommended
+				})		
 		except Stock.DoesNotExist:
 			return HttpResponse("<p>error</p>")
 	else:
