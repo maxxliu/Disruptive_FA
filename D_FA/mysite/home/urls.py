b660edf89df2bbd31abd35d826ea4f5acf5aa9f2
Jonathan Li <jonathanl1@cs.uchicago.edu>
2017-03-06 20:49:03 -0600

    working on info pages

--- D_FA/mysite/home/urls.py
+++ D_FA/mysite/home/urls.py
@@ -4,6 +4,14 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^info/$', views.info, name='info'),
+    url(r'^step_1/$', views.step_1, name='step_1'),
+    url(r'^step_2/$', views.step_2, name='step_2'),
+    url(r'^step_3/$', views.step_3, name='step_3'),
+    url(r'^step_4/$', views.step_4, name='step_4'),
+    url(r'^step_5/$', views.step_5, name='step_5'),
+    url(r'^step_6/$', views.step_6, name='step_6'),
+    url(r'^step_7/$', views.step_7, name='step_7'),
+    url(r'^step_8/$', views.step_8, name='step_8'),
     url(r'^quick_search/$', views.quick_search, name='quick_search'),
     url(r'^advanced_search/$', views.advanced_search, name='advanced_search')
 ]
--- D_FA/mysite/home/views.py
+++ D_FA/mysite/home/views.py
@@ -15,3 +15,35 @@ def advanced_search(request):
 
 def info(request):
 	return render(request, 'home/info.html')
+
+
+def step_1(request):
+	return render(request, 'home/step_1.html')
+
+
+def step_2(request):
+	return render(request, 'home/step_2.html')
+
+
+def step_3(request):
+	return render(request, 'home/step_3.html')
+
+
+def step_4(request):
+	return render(request, 'home/step_4.html')
+
+
+def step_5(request):
+	return render(request, 'home/step_5.html')
+
+
+def step_6(request):
+	return render(request, 'home/step_6.html')
+
+
+def step_7(request):
+	return render(request, 'home/step_7.html')
+
+
+def step_8(request):
+	return render(request, 'home/step_8.html')
